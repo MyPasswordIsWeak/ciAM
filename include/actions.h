30906d194760197d0d3c77fc7fff72e37c92eb76
TimmSkiller <timmtimm1234567890@gmail.com>
2020-11-18 13:08:02 +0300

    plz halb me bad dev

--- include/actions.h
+++ include/actions.h
@@ -10,8 +10,10 @@
 
 #include "libs.h"
 
-void draw_page(int offset, int pageSize, u32 tidcount, u64 *tids);
+void draw_page(int offset, int pageSize, u32 tidcount, AM_TitleEntry *entries);
 int uninstaller_menu(void);
 int installer_menu(void);
+int delete_title(bool deleteticket, u64 titleid);
+int load_titles(FS_MediaType mediaType, u32 *titleCount, u64 *titleIds, AM_TitleEntry *titleEntries);
 
 #endif
